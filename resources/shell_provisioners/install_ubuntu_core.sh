8c02fd723b2dde5182a79c30e1b804132d52930d
Ted Zlatanov <tzz@lifelogs.com>
2014-03-05 14:47:08 -0500

    fix up Ubuntu compilation to use LMDB

--- resources/shell_provisioners/install_ubuntu_core.sh
+++ resources/shell_provisioners/install_ubuntu_core.sh
@@ -7,7 +7,11 @@ test -d /var/cfengine && exit 0
 
 cd /var/tmp
 apt-get -qq update
-apt-get -qq -y install git autoconf libtool libtokyocabinet-dev libpcre3-dev libxml2-dev libvirt-dev libacl1-dev bison flex make libssl-dev
+apt-get -qq -y install git autoconf libtool libtokyocabinet-dev libpcre3-dev libxml2-dev libvirt-dev libacl1-dev bison flex make libssl-dev libpam-dev
+
+git clone -q --depth 1 https://git.gitorious.org/mdb/mdb.git && cd mdb/libraries/liblmdb && make install
+
+cd /var/tmp
 
 if [ -z "$1" ]; then
     git clone -q --depth 1 $URL ./core
@@ -15,7 +19,4 @@ else
     git clone -q --depth 1 --branch $1 $URL ./core
 fi
 
-cd ./core
-./autogen.sh
-make
-make install
+cd /var/tmp/core && ./autogen.sh && make && make install
